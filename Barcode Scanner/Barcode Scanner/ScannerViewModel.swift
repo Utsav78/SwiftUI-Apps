3822a303ef7cad75135d4ae002ba7138c93deb0f
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-04-30 23:34:37 +0930

    Add Viewmodel

---
+++ Barcode Scanner/Barcode Scanner/ScannerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerViewModel.swift
+//  Barcode Scanner
+//
+//  Created by Utsav Budathoki on 30/4/2024.
+//
+
+import SwiftUI
+
+struct ScannerViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerViewModel()
+}
