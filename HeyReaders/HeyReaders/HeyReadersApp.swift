660db75df9a9e93b4e4b0217190ef1a411cad70a
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-05-02 16:23:36 +0930

    HeyReaders App setup

---
+++ HeyReaders/HeyReaders/HeyReadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyReadersApp.swift
+//  HeyReaders
+//
+//  Created by Utsav Budathoki on 2/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct HeyReadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
