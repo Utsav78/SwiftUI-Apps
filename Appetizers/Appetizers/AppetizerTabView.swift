b229caa2bc466915e78a8d51da6b50ea66027ae2
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-05-10 23:30:56 +0930

    fix: image loading in tabbar

--- Appetizers/Appetizers/AppetizerTabView.swift
+++ Appetizers/Appetizers/AppetizerTabView.swift
@@ -12,19 +12,19 @@ struct AppetizerTabView: View {
         TabView {
             AppetizerListView()
                 .tabItem {
-                    Image("house")
+                    Image(systemName: "house")
                     Text("Home")
                 }
             
             AccountView()
                 .tabItem {
-                    Image("person")
+                    Image(systemName: "person")
                     Text("Account")
                 }
             
             OrderView()
                 .tabItem {
-                    Image("bag")
+                    Image(systemName: "bag")
                     Text("Order")
                 }
         }
