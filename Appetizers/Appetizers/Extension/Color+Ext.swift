280fdd88145f3a40f9289a8fd52ae473eb5e6b77
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-06-07 19:44:26 +0930

    Add Extension

---
+++ Appetizers/Appetizers/Extension/Color+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Utsav Budathoki on 7/6/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
+
