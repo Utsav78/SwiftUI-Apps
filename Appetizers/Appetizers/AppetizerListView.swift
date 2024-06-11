6572b8dbe1d7ad70c990d374fda336d5eb9c085d
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-06-11 16:24:24 +0930

    orderview, appetizerlistviewmodel, and appetizerlistview added

--- Appetizers/Appetizers/AppetizerListView.swift
+++ Appetizers/Appetizers/AppetizerListView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @StateObject var viewModel = AppetizerListViewModel()
+    
     var body: some View {
-        NavigationView {
-            List(Mockdata.appetizers) { appetizer in
-                Text(appetizer.name)
-                
+        ZStack {
+            NavigationView {
+                List(viewModel.appetizers) { appetizer in
+                    AppetizerListCell(appetizer: appetizer)
+                        .listRowSeparator(.hidden)
+//                        .listRowSeparatorTint(.brandPrimary)
+                        .onTapGesture {
+                            viewModel.selectedAppetizer = appetizer
+                            viewModel.isShowingDetail = true
+                        }
+                }
+                .navigationTitle("🍟 Appetizers")
+                .listStyle(.plain)
+                .disabled(viewModel.isShowingDetail)
             }
-            .navigationTitle("🍟 Appetizers")
+            .task {
+                viewModel.getAppetizers()
+            }
+            .blur(radius: viewModel.isShowingDetail ? 20 : 0)
+            
+            if viewModel.isShowingDetail {
+                AppetizerDetailView(appetizer: viewModel.selectedAppetizer!,
+                                    isShowingDetail: $viewModel.isShowingDetail)
+            }
+            
+            if viewModel.isLoading {
+                LoadingView()
+            }
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
 
-#Preview {
-    AppetizerListView()
+struct AppetizerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListView()
+    }
 }
--- Appetizers/Appetizers/OrderView.swift
+++ Appetizers/Appetizers/OrderView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @EnvironmentObject var order: Order
+    
+    
     var body: some View {
         NavigationView {
-            Text("🛍️ Order")
-                .navigationTitle("Order")
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(order.items) { appetizer in
+                            AppetizerListCell(appetizer: appetizer)
+                        }
+                        .onDelete(perform: order.deleteItems)
+                    }
+                    .listStyle(PlainListStyle())
+                    
+                    Button {
+                        print("order placed")
+                    } label: {
+//                        APButton(title: "$\(order.totalPrice, specifier: "%.2f") - Place Order")
+                        Text("$\(order.totalPrice, specifier: "%.2f") - Place Order")
+                    }
+                    .modifier(StandardButtonStyle())
+                    .padding(.bottom, 25)
+                }
+                
+                if order.items.isEmpty {
+                    EmptyState(imageName: "empty-order",
+                               message: "You have no items in your order.\nPlease add an appetizer!")
+                }
+            }
+            .navigationTitle("🧾 Orders")
         }
     }
 }
 
-#Preview {
-    OrderView()
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
 }
